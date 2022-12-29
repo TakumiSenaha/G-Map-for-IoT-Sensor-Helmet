d2bae17353b29a4c625b31f42e30cc3a85dea70c
TakumiSenaha <117294735+TakumiSenaha@users.noreply.github.com>
2022-12-29 21:02:54 +0900

    Add files via upload

---
+++ g_map/constants.js
@@ -0,0 +1,4 @@
+//for create sheet2gmap at https://github.com/champierre/sheet2gmap/blob/master/constants.js
+
+const API_KEY = 'AIzaSyBSKSbBMF-rQ4VtKU9kSHs7Nwt_38tpt0E';
+const SHEET_ID = '1fDsMCqDCY61gp_hn_uEdMqr-ho85GtrsPeqxvmcgymI';
