c091eadcc619492684b149a868f820af86136065
Takumi Senaha <117294735+TakumiSenaha@users.noreply.github.com>
2023-05-26 21:49:57 +0900

    Update constants.js

--- g_map_locus/constants.js
+++ g_map_locus/constants.js
@@ -1,4 +1,4 @@
 //for create sheet2gmap at https://github.com/champierre/sheet2gmap/blob/master/constants.js
 
-const API_KEY = 'AIzaSyBSKSbBMF-rQ4VtKU9kSHs7Nwt_38tpt0E';
-const SHEET_ID = '1KsyeVj5Nof3xLbT-DcXTr8y5mpFPRqvVVb2T-1ZwdoU';
+const API_KEY = '';
+const SHEET_ID = '';
