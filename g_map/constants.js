0fdc421201dc3ad16bd6ce20d22f2330084655fa
Takumi Senaha <117294735+TakumiSenaha@users.noreply.github.com>
2023-05-26 21:48:58 +0900

    Update constants.js

--- g_map/constants.js
+++ g_map/constants.js
@@ -1,4 +1,4 @@
 //for create sheet2gmap at https://github.com/champierre/sheet2gmap/blob/master/constants.js
 
-const API_KEY = 'AIzaSyBSKSbBMF-rQ4VtKU9kSHs7Nwt_38tpt0E';
-const SHEET_ID = '1fDsMCqDCY61gp_hn_uEdMqr-ho85GtrsPeqxvmcgymI';
+const API_KEY = '';
+const SHEET_ID = '';
